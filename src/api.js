2e199242f54b6bb99b3c4ce7d42df8f0b03064af
DimaOliynyk <d.oliynyk@outlook.com>
2025-08-14 19:58:00 +0200

    change api link

--- src/api.js
+++ src/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 // Base URL
-const API_BASE_URL = process.env.REACT_APP_API_BASE_URL;
+const API_BASE_URL = 'https://cash-app-server-cydp.onrender.com/api';
 
 
 export const loginUser = async ({ username, password }) => {
